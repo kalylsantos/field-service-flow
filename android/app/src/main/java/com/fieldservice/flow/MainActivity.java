c42615e717a731e273175b547a32215dc1e8c2dd
kalylx <kalyl.santos@outlook.com>
2025-12-21 21:26:18 -0300

    ask for permission

---
+++ android/app/src/main/java/com/fieldservice/flow/MainActivity.java
@@ -0,0 +1,5 @@
+package com.fieldservice.flow;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.fieldservice.flow',
+  appName: 'field-service-flow',
+  webDir: 'dist'
+};
+
+export default config;
